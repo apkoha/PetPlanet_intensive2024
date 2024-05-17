5e68b6846c6cd49d090be037aae8ac0667a09d4a
apkoha <79258179+apkoha@users.noreply.github.com>
2024-05-17 09:54:20 +0300

    day 1 store.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".subscribe__input");
 const button = document.querySelector(".subscribe__button");
+const buttons = document.querySelectorAll("store__category-button");
 
 input.addEventListener("focus", () => {
   input.dataset.placeholder = input.placeholder;
@@ -10,7 +11,21 @@ input.addEventListener("blur", () => {
   input.placeholder = input.dataset.placeholder;
 });
 
-//блокирует кнопку отправить при пустом инпут.
+//блокирует кнопку "отправить" при пустом инпут.
 input.addEventListener("input", ({ target }) => {
   button.disabled = !target.value.trim();
+});
+
+//добавляет класс "store__category-button_active" по клику на кнопку
+const changeActiveBtn = (event) => {
+  const target = event.target;
+  buttons.forEach((button) => {
+    button.classList.remove("store__category-button_active");
+  });
+
+  target.classList.add("store__category-button_active");
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", changeActiveBtn);
 });
