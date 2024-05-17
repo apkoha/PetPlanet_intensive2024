56b07d87f0a67a19d9676ef20aaf7ef9edaca274
apkoha <79258179+apkoha@users.noreply.github.com>
2024-05-17 20:23:10 +0300

    day 3 cart js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 // const input = document.querySelector(".subscribe__input");
 // const button = document.querySelector(".subscribe__button");
+const API_URL = "https://cooked-mirage-crayon.glitch.me";
+
+const buttons = document.querySelectorAll(".store__category-button");
+const productList = document.querySelector(".store__list");
+const cartButton = document.querySelector(".store__cart-button");
+const cartCount = cartButton.querySelector(".store__cart-cnt");
+const modalOverlay = document.querySelector(".modal-overlay");
+const cartItemsList = document.querySelector(".modal__cart-items");
+const modalCloseButton = document.querySelector(".modal-overlay_close-button");
 
 // input.addEventListener("focus", () => {
 //   input.dataset.placeholder = input.placeholder;
@@ -15,24 +24,6 @@
 //   button.disabled = !target.value.trim();
 // });
 
-const buttons = document.querySelectorAll(".store__category-button");
-
-//добавляет класс "store__category-button_active" по клику на кнопку
-const changeActiveBtn = (event) => {
-  const target = event.target;
-  buttons.forEach((button) => {
-    button.classList.remove("store__category-button_active");
-  });
-
-  target.classList.add("store__category-button_active");
-};
-
-buttons.forEach((button) => {
-  button.addEventListener("click", changeActiveBtn);
-});
-
-const API_URL = "https://cooked-mirage-crayon.glitch.me";
-const productList = document.querySelector(".store__list");
 const createProductCard = (product) => {
   const productCard = document.createElement("li");
   productCard.classList.add("store__item");
@@ -73,4 +64,70 @@ const fetchProductByCategory = async (category) => {
   }
 };
 
-fetchProductByCategory("Домики");
+//добавляет класс "store__category-button_active" по клику на кнопку
+const changeCategory = ({ target }) => {
+  const category = target.textContent;
+  console.log("category: ", category);
+
+  buttons.forEach((button) => {
+    button.classList.remove("store__category-button_active");
+  });
+
+  target.classList.add("store__category-button_active");
+  fetchProductByCategory(category);
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", changeCategory);
+
+  if (button.classList.contains("store__category-button_active")) {
+    fetchProductByCategory(button.textContent);
+  }
+});
+
+const renderCartItems = () => {
+  cartItemsList.textContent = "";
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+
+  cartItems.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item;
+    cartItemsList.append(listItem);
+  });
+};
+
+//отображение модального окна
+cartButton.addEventListener("click", () => {
+  modalOverlay.style.display = "flex";
+  renderCartItems();
+});
+
+//закрытие модального окна
+modalOverlay.addEventListener("click", ({ target }) => {
+  if (
+    target === modalOverlay ||
+    target.closest(".modal-overlay_close-button")
+  ) {
+    modalOverlay.style.display = "none";
+  }
+});
+
+const updateCartCount = () => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+  cartCount.textContent = cartItems.length;
+};
+
+updateCartCount();
+
+const addToCart = (productName) => {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+  cartItems.push(productName);
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  updateCartCount();
+};
+
+productList.addEventListener("click", ({ target }) => {
+  const productCard = target.closest(".store__product");
+  const productName = productCard.querySelector(".product__title").textContent;
+  addToCart(productName);
+});
